d48ceece96e98891a7bfdcec4fb6680a37bf0dda
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-17 10:46:14 +0800

    kj

--- example_18/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
+++ example_18/src/main/java/com/eazybytes/eazyschool/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
+    @RequestMapping(value={"/home","/",""})
     public String displayHomePage() {
     	System.out.println("now in display home page method");
         return "testing.html";

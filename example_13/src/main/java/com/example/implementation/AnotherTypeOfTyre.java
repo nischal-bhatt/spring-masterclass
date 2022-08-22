0e057c70dae3117e07ce9b0ebbe1cbf9558e9a13
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-22 17:16:10 +0800

    context.getBeanNamesForType example

---
+++ example_13/src/main/java/com/example/implementation/AnotherTypeOfTyre.java
@@ -0,0 +1,16 @@
+package com.example.implementation;
+
+import org.springframework.stereotype.Component;
+
+import com.example.interfaces.Tyres;
+
+@Component
+public class AnotherTypeOfTyre implements Tyres{
+
+	@Override
+	public String rotate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- example_13/src/main/java/com/example/main/Example13.java
+++ example_13/src/main/java/com/example/main/Example13.java
@@ -3,6 +3,8 @@ package com.example.main;
 import com.example.beans.Person;
 import com.example.beans.Vehicle;
 import com.example.config.ProjectConfig;
+import com.example.interfaces.Tyres;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Example13 {
@@ -12,10 +14,21 @@ public class Example13 {
         String[] persons = context.getBeanNamesForType(Person.class);
         Person person = context.getBean(Person.class);
         String[] names = context.getBeanNamesForType(Vehicle.class);
+        String[] allnames = context.getBeanNamesForType(Tyres.class);
         /*Vehicle vehicle = context.getBean(Vehicle.class);
         vehicle.getVehicleServices().playMusic();
         vehicle.getVehicleServices().moveVehicle();*/
         person.getVehicle().getVehicleServices().playMusic();
         person.getVehicle().getVehicleServices().moveVehicle();
+        System.out.println(persons);
+        System.out.println(names);
+        System.out.println();
+        
+        System.out.println(allnames.length);
+        
+        for (int i =0; i<allnames.length; i++)
+        {
+        	System.out.println(allnames[i].toString());
+        }
     }
 }

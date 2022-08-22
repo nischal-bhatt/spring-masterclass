d1cbe579caf90b9cd333d0ee415136afbf53b817
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-22 14:35:35 +0800

    part 1 - introduction to spring and this course

--- example_10/src/main/java/com/example/main/Example10.java
+++ example_10/src/main/java/com/example/main/Example10.java
@@ -9,6 +9,44 @@ public class Example10 {
 
     public static void main(String[] args) {
 
+    	//spring
+    	//springboot
+    	//jpa
+    	//hibernate
+    	
+    	/*
+    	 *  what is spring
+    	 *  spring vs java ee
+    	 *  evolution of spring
+    	 *  spring releases
+    	 *  different spring projects
+    	 *  
+    	 *  spring core
+    	 *  IOC, AOP, DI etc etc 
+    	 *  
+    	 *  spring mvc
+    	 *  thymeleaf - 
+    	 *  
+    	 *  what is spring boot ? what is the diff between spring boot and springmvc
+    	 *  autoconfigs etc
+    	 *  
+    	 *  spring security 
+    	 *  spring jdbc
+    	 *  spring data
+    	 *  spring rest
+    	 *  logging
+    	 *  properties and profiles
+    	 *  spring boot actuator
+    	 *  
+    	 *  sample web application - school 
+    	 *  school - very close to our hearts 
+    	 *  
+    	 *  pdf of the course - 
+    	 *  github.com/eazybytes/spring - 50 example projects
+    	 *  there will be pdf for the course - notes
+    	 *  
+    	 */
+    	
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Person person = context.getBean(Person.class);
        // Vehicle vehicle = context.getBean(Vehicle.class);
